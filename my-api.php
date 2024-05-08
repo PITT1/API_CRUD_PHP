bb1b83d4a078e1aa46dd0d17cbbc0bdcfc7e88ab
PITT1 <petroleo.plta.97@gmail.com>
2024-05-07 21:41:38 -0400

    agregando CORS

--- my-api.php
+++ my-api.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+
 $servername = "localhost";
 $username = "root";
 $password = "";
